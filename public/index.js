6b9bbc97e79011b2e972dbdba45b1bfc7ef7efde
Jennika Wilson <j.r.a.112100@gmail.com>
2022-04-06 20:41:54 -0400

    #1 done

--- public/index.js
+++ public/index.js
@@ -12,7 +12,33 @@ async function main() {
     const { GME, MSFT, DIS, BNTX } = result
     const stocks = [GME, MSFT, DIS, BNTX]
 
-    console.log(result, stocks)
-}
+    stocks.forEach(stock => stock.values.reverse())
 
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }          
+}
 main()
